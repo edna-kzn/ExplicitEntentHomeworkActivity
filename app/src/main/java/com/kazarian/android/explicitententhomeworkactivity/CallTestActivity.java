a232c4f1bb5131c52f587fa6d7e57900c5d7e48a
edna-kzn <77274244+edna-kzn@users.noreply.github.com>
2021-01-28 13:42:22 +0330

    second homework jalase4 is completed

--- app/src/main/java/com/kazarian/android/explicitententhomeworkactivity/CallTestActivity.java
+++ app/src/main/java/com/kazarian/android/explicitententhomeworkactivity/CallTestActivity.java
@@ -2,7 +2,13 @@ package com.kazarian.android.explicitententhomeworkactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CallTestActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class CallTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_test);
+
+        EditText edtphonenumber = findViewById(R.id.edtPhoneNumber);
+        Button btncall = findViewById(R.id.btnCall);
+
+
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phonenumber = edtphonenumber.getText().toString();
+                Intent callintent = new Intent(Intent.ACTION_DIAL);
+                callintent.setData(Uri.parse("tel:" + phonenumber));
+                startActivity(callintent);
+            }
+
+        });
     }
 }
