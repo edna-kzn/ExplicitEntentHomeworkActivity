c156cbc521cc7b1b281af88499a6f7d4b1d79a56
edna-kzn <77274244+edna-kzn@users.noreply.github.com>
2021-01-28 13:36:41 +0330

    first homework jalase4 is complete

---
+++ app/src/main/java/com/kazarian/android/explicitententhomeworkactivity/CallTestActivity.java
@@ -0,0 +1,14 @@
+package com.kazarian.android.explicitententhomeworkactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_test);
+    }
+}
